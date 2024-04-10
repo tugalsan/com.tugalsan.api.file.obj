79a79f1632706765d1de7d331654842163ab2047
tugalsan karabacak <tugalsan@users.noreply.github.com>
2024-04-10 12:51:39 +0300

    Update TS_FileObjUtils.java

--- src/main/java/com/tugalsan/api/file/obj/server/TS_FileObjUtils.java
+++ src/main/java/com/tugalsan/api/file/obj/server/TS_FileObjUtils.java
@@ -1,39 +1,39 @@
 package com.tugalsan.api.file.obj.server;
 
 import com.tugalsan.api.string.server.*;
-import com.tugalsan.api.union.client.TGS_Union;
+import com.tugalsan.api.union.client.TGS_UnionExcuse;
 import java.io.*;
 
 public class TS_FileObjUtils {
 
-    public static TGS_Union<byte[]> toBytes(Object obj) {
+    public static TGS_UnionExcuse<byte[]> toBytes(Object obj) {
         if (obj == null) {
-            return TGS_Union.ofExcuse(
+            return TGS_UnionExcuse.ofExcuse(
                     TS_FileObjUtils.class.getSimpleName(),
                     "toBytes(Object obj)",
                     "obj == null"
             );
         }
         if (obj instanceof byte[] val) {
-            return TGS_Union.of(val);
+            return TGS_UnionExcuse.of(val);
         }
         if (obj instanceof CharSequence val) {
-            return TGS_Union.of(TS_StringUtils.toByte(val.toString()));
+            return TGS_UnionExcuse.of(TS_StringUtils.toByte(val.toString()));
         }
         try (var baos = new ByteArrayOutputStream()) {
             try (var oos = new ObjectOutputStream(baos)) {// DO NOT CLOSE OOS before getting byte array!
                 oos.writeObject(obj);
                 oos.flush();
-                return TGS_Union.of(baos.toByteArray());
+                return TGS_UnionExcuse.of(baos.toByteArray());
             }
         } catch (IOException ex) {
-            return TGS_Union.ofExcuse(ex);
+            return TGS_UnionExcuse.ofExcuse(ex);
         }
     }
 
-    public static <T> TGS_Union<T> toObject(byte[] bytes, Class<T> outputType) {//java.io.StreamCorruptedException: invalid stream header: 312D2041'
+    public static <T> TGS_UnionExcuse<T> toObject(byte[] bytes, Class<T> outputType) {//java.io.StreamCorruptedException: invalid stream header: 312D2041'
         if (bytes == null) {
-            return TGS_Union.ofExcuse(
+            return TGS_UnionExcuse.ofExcuse(
                     TS_FileObjUtils.class.getSimpleName(),
                     "toObject(byte[] bytes, Class<T> outputType)",
                     "bytes == null"
@@ -41,18 +41,18 @@ public class TS_FileObjUtils {
         }
         if (outputType == CharSequence.class || outputType == String.class) {
             var str = TS_StringUtils.toString(bytes);
-            return TGS_Union.of((T) str);
+            return TGS_UnionExcuse.of((T) str);
         }
         try (var bais = new ByteArrayInputStream(bytes)) {
             return toObject(bais, outputType);
         } catch (IOException ex) {
-            return TGS_Union.ofExcuse(ex);
+            return TGS_UnionExcuse.ofExcuse(ex);
         }
     }
 
-    public static <T> TGS_Union<T> toObject(InputStream is, Class<T> outputType) {
+    public static <T> TGS_UnionExcuse<T> toObject(InputStream is, Class<T> outputType) {
         if (is == null) {
-            return TGS_Union.ofExcuse(
+            return TGS_UnionExcuse.ofExcuse(
                     TS_FileObjUtils.class.getSimpleName(),
                     "toObject(InputStream is, Class<T> outputType) ",
                     "is == null"
@@ -60,21 +60,21 @@ public class TS_FileObjUtils {
         }
         if (outputType == CharSequence.class || outputType == String.class) {
             var str = TS_StringUtils.toString(is);
-            return TGS_Union.of((T) str);
+            return TGS_UnionExcuse.of((T) str);
         }
         Object obj;
         try (var input = new ObjectInputStream(is)) {
             obj = input.readObject();
             if (obj == null && !outputType.isInstance(obj)) {
-                return TGS_Union.ofExcuse(
+                return TGS_UnionExcuse.ofExcuse(
                         TS_FileObjUtils.class.getSimpleName(),
                         "toObject(InputStream is, Class<T> outputType) ",
                         "bj == null && !outputType.isInstance(obj)"
                 );
             }
-            return TGS_Union.of((T) obj);
+            return TGS_UnionExcuse.of((T) obj);
         } catch (IOException | ClassNotFoundException e) {
-            return TGS_Union.ofExcuse(e);
+            return TGS_UnionExcuse.ofExcuse(e);
         }
     }
 }
