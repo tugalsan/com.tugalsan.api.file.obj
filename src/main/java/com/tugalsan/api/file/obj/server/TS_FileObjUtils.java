5edfadc1f1e7b8fa88cdf6d9f46b911a7bfe8c52
tugalsan karabacak <tugalsan@users.noreply.github.com>
2024-05-14 07:58:00 +0300

    .

--- src/main/java/com/tugalsan/api/file/obj/server/TS_FileObjUtils.java
+++ src/main/java/com/tugalsan/api/file/obj/server/TS_FileObjUtils.java
@@ -1,43 +1,43 @@
 package com.tugalsan.api.file.obj.server;
 
 import com.tugalsan.api.string.server.*;
+import com.tugalsan.api.union.client.TGS_UnionExcuse;
 import com.tugalsan.api.unsafe.client.*;
 import java.io.*;
-import java.util.Optional;
 
 public class TS_FileObjUtils {
 
-    public static Optional<byte[]> toBytes(Object obj) {
+    public static TGS_UnionExcuse<byte[]> toBytes(Object obj) {
         return TGS_UnSafe.call(() -> {
             if (obj == null) {
-                return Optional.empty();
+                return TGS_UnionExcuse.ofExcuse(TS_FileObjUtils.class.getSimpleName(), "toBytes", "obj == null");
             }
             if (obj instanceof byte[] val) {
-                return Optional.of(val);
+                return TGS_UnionExcuse.of(val);
             }
             if (obj instanceof CharSequence val) {
-                return Optional.of(TS_StringUtils.toByte(val.toString()));
+                return TGS_UnionExcuse.of(TS_StringUtils.toByte(val.toString()));
             }
             try (var baos = new ByteArrayOutputStream()) {
                 try (var oos = new ObjectOutputStream(baos)) {// DO NOT CLOSE OOS before getting byte array!
                     oos.writeObject(obj);
                     oos.flush();
-                    return Optional.of(baos.toByteArray());
+                    return TGS_UnionExcuse.of(baos.toByteArray());
                 }
             } catch (IOException ex) {
-                return Optional.empty();
+                return TGS_UnionExcuse.ofExcuse(ex);
             }
         });
     }
 
-    public static <T> Optional<T> toObject(byte[] bytes, Class<T> outputType) {//java.io.StreamCorruptedException: invalid stream header: 312D2041'
+    public static <T> TGS_UnionExcuse<T> toObject(byte[] bytes, Class<T> outputType) {//java.io.StreamCorruptedException: invalid stream header: 312D2041'
         return TGS_UnSafe.call(() -> {
             if (bytes == null) {
-                return Optional.empty();
+                return TGS_UnionExcuse.ofExcuse(TS_FileObjUtils.class.getSimpleName(), "toObject", "bytes == null");
             }
             if (outputType == CharSequence.class || outputType == String.class) {
                 var str = TS_StringUtils.toString(bytes);
-                return Optional.of((T) str);
+                return TGS_UnionExcuse.of((T) str);
             }
             try (var bais = new ByteArrayInputStream(bytes)) {
                 return toObject(bais, outputType);
@@ -45,24 +45,24 @@ public class TS_FileObjUtils {
         });
     }
 
-    public static <T> Optional<T> toObject(InputStream is, Class<T> outputType) {
+    public static <T> TGS_UnionExcuse<T> toObject(InputStream is, Class<T> outputType) {
         return TGS_UnSafe.call(() -> {
             if (is == null) {
-                return Optional.empty();
+                return TGS_UnionExcuse.ofExcuse(TS_FileObjUtils.class.getSimpleName(), "toObject", "is == null");
             }
             if (outputType == CharSequence.class || outputType == String.class) {
                 var str = TS_StringUtils.toString(is);
-                return Optional.of((T) str);
+                return TGS_UnionExcuse.of((T) str);
             }
             Object obj;
             try (var input = new ObjectInputStream(is)) {
                 obj = input.readObject();
                 if (obj == null && !outputType.isInstance(obj)) {
-                    return Optional.empty();
+                    return TGS_UnionExcuse.ofExcuse(TS_FileObjUtils.class.getSimpleName(), "toObject", "obj == null && !outputType.isInstance(obj)");
                 }
-                return Optional.of((T) obj);
+                return TGS_UnionExcuse.of((T) obj);
             } catch (EOFException e) {
-                return Optional.empty();
+                return TGS_UnionExcuse.ofExcuse(e);
             }
         });
     }
