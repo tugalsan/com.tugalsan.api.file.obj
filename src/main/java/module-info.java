7d336488761779671f38336cc51a2a734c0ffa21
tugalsan karabacak <tugalsan@users.noreply.github.com>
2024-06-27 12:02:53 +0300

    .

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,6 @@ module com.tugalsan.api.file.obj {
     requires com.tugalsan.api.unsafe;
     requires com.tugalsan.api.union;
     requires com.tugalsan.api.string;
-    requires com.tugalsan.api.callable;
+    requires com.tugalsan.api.function;
     exports com.tugalsan.api.file.obj.server;
 }
